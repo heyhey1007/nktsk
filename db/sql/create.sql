9c28ad21b5211ca27b25ad456e1ce3dcdfcd6e9a
nakata <54203719+heyhey1007@users.noreply.github.com>
2021-10-23 10:36:12 +0900

    テーブル定義追加

---
+++ db/sql/create.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `nakatask` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
